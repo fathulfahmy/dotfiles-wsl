2424ad2e23d338e6270e0088f7730555954d6516
Fathul <100550389+fathulfahmy@users.noreply.github.com>
2023-06-13 13:30:32 +0800

    Create windows-install.ps1

---
+++ install/windows-install.ps1
@@ -0,0 +1,6 @@
+winget install --id=Microsoft.WindowsTerminal.Preview -e  ; 
+winget install --id=Microsoft.VisualStudioCode.Insiders -e  ; 
+winget install --id=Brave.Brave -e ;
+winget install lars-berger.GlazeWM ;
+winget install --id Git.Git -e --source winget ;
+wsl --install ;
