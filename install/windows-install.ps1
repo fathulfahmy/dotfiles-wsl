ab53897e27ad7275aea85a5b0ce9a012a2ad10c4
Patui <fathulfmy@gmail.com>
2023-07-14 11:33:37 +0800

    feat: delete install directory

--- install/windows-install.ps1
+++
@@ -1,7 +0,0 @@
-winget install --id=Microsoft.VCRedist.2015+.x64 -e ; 
-winget install --id=Microsoft.WindowsTerminal.Preview -e  ; 
-winget install --id=Microsoft.VisualStudioCode.Insiders -e  ; 
-winget install --id=Brave.Brave -e ;
-winget install lars-berger.GlazeWM ;
-winget install --id Git.Git -e --source winget ;
-wsl --install ;
